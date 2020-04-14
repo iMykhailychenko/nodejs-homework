9294c81409bba6c76392170ff1f1680da545fe64
imykhailychenko <igor.c.m@ukr.net>
2020-04-14 20:13:14 +0300

    callbacks rewrited to promise

--- homework-01/contacts.js
+++ homework-01/contacts.js
@@ -1,36 +1,29 @@
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
 const contactsPath = path.join(__dirname, './db/contacts.json');
 
-
-function getContactsFromFile(callback) {
-  fs.readFile(contactsPath, 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const parsedData = JSON.parse(data);
-    callback(parsedData);
-  });
+function getContactsFromFile() {
+  return fsPromises
+    .readFile(contactsPath, 'utf8')
+    .then(contacts => JSON.parse(contacts))
+    .catch(err => console.log(err));
 }
 
 function listContacts() {
-  getContactsFromFile(console.log);
+  getContactsFromFile().then(data => console.log(data));
 }
 
 function getContactById(contactId) {
-  const logContact = contacts => {
+  getContactsFromFile().then(contacts => {
     const contactById = contacts.find(contact => contact.id === contactId);
     const contact = contactById || 'Не найдено контактов по заданому id';
     console.log(contact);
-  };
-
-  getContactsFromFile(logContact);
+  });
 }
 
 function removeContact(contactId) {
-  const removeContactFromFile = contacts => {
+  getContactsFromFile().then(contacts => {
     const contactInDB = contacts.some(contact => contact.id === contactId);
     if (!contactInDB) {
       console.log('Не найдено контактов по заданому id');
@@ -42,14 +35,12 @@ function removeContact(contactId) {
     );
     const contactsToStr = JSON.stringify(clearedContacts, null, 2);
 
-    fs.writeFile(contactsPath, contactsToStr, 'utf8', err => {
-      if (err) throw err;
-    });
+    fsPromises
+      .writeFile(contactsPath, contactsToStr, 'utf8')
+      .catch(err => console.log(err));
 
     console.log('done');
-  };
-
-  getContactsFromFile(removeContactFromFile);
+  });
 }
 
 function addContact(name, email, phone) {
@@ -61,18 +52,16 @@ function addContact(name, email, phone) {
     phone,
   };
 
-  const addContactToFile = contacts => {
+  getContactsFromFile().then(contacts => {
     const newContacts = [...contacts, contact];
     const newContactsStr = JSON.stringify(newContacts, null, 2);
 
-    fs.writeFile(contactsPath, newContactsStr, 'utf8', err => {
-      if (err) throw err;
-    });
+    fsPromises
+      .writeFile(contactsPath, newContactsStr, 'utf8')
+      .catch(err => console.log(err));
 
     console.log('done');
-  };
-
-  getContactsFromFile(addContactToFile);
+  });
 }
 
 module.exports = {
